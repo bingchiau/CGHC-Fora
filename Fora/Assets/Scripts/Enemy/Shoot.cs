cd6379fd842e6abb0722a9c8b5f983972262b85f
BingChiau <118031847+bingchiau@users.noreply.github.com>
2025-07-21 13:57:38 +0800

    Hamster basic set up

--- Fora/Assets/Scripts/Enemy/Shoot.cs
+++ Fora/Assets/Scripts/Enemy/Shoot.cs
@@ -6,10 +6,16 @@ public class Shoot : StateMachineBehaviour
 {
     [Header("Settings")]
     [SerializeField] private GameObject _nuts;
+    [SerializeField] private float _cooldown;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Instantiate(_nuts, animator.transform.position, Quaternion.identity);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetFloat("attackCountdown", _cooldown);
     }
 }
